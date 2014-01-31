fad7043cf7a0c507b0b7f8d51094cedaf5776c17
NicoSantangelo <santangelonicolas@gmail.com>
2014-01-31 17:49:21 -0300

    First tests

---
+++ tests/test_card_options.py
@@ -0,0 +1,24 @@
+import unittest
+from .mock import MagicMock
+
+from card_options import CardOptions
+
+class CardOptionsTests(unittest.TestCase):
+    def setUp(self):
+        fake_card = {}
+        fake_command = {}
+        self.card_options = CardOptions(fake_card, fake_command)
+
+    def test_names_returns_the_card_operations(self):
+        self.assertEqual(["Comment", "Archive", "Exit"], self.card_options.names())
+
+    def test_execute_calls_the_method_at_the_given_index(self):
+        first_option = self.card_options.options[1]
+        first_option["action"] = MagicMock()
+
+        self.card_options.execute(1)
+        first_option["action"].assert_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
