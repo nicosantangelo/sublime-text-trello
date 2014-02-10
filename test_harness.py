7f88cf0efb2044151365fb391965e848c3c9ee26
NicoSantangelo <santangelonicolas@gmail.com>
2014-02-10 18:23:08 -0300

    Fix AAAPT

--- test_harness.py
+++ test_harness.py
@@ -2,8 +2,9 @@ from AAAPT.runner import register_tests
 
 test_suites = {
     'card_options': ['Trello.tests.test_card_options'],
-    'navigator': ['Trello.tests.test_navigator'],
-    'operations': ['Trello.tests.test_operations']
+    'executable'  : ['Trello.tests.test_executable'],
+    'operations'  : ['Trello.tests.test_operations'],
+    'comment_formatter': ['Trello.tests.test_comment_formatter']
 }
 
 register_tests(test_suites)
