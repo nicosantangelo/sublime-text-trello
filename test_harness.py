bed8b915fd07ac6804c120cff6c457230b71785a
NicoSantangelo <santangelonicolas@gmail.com>
2014-04-12 17:03:01 -0300

    Don't fail if AAAPT isn't present

--- test_harness.py
+++ test_harness.py
@@ -1,10 +1,11 @@
-from AAAPT.runner import register_tests
-
-test_suites = {
-    'card_options': ['Trello.tests.test_card_options'],
-    'executable'  : ['Trello.tests.test_executable'],
-    'operations'  : ['Trello.tests.test_operations'],
-    'output'      : ['Trello.tests.test_output']
-}
-
-register_tests(test_suites)
+try:
+    from AAAPT.runner import register_tests
+    register_tests({
+        'card_options': ['Trello.tests.test_card_options'],
+        'executable'  : ['Trello.tests.test_executable'],
+        'operations'  : ['Trello.tests.test_operations'],
+        'output'      : ['Trello.tests.test_output']
+    })
+except ImportError:
+    print("Install the AAAPT Package to test Trello from SublimeText")
+    
